78998e132aabc0c41acc010310809698beb8e42a
Jakob Dhondt <jakob.dhondt@switch.ch>
2018-08-07 11:52:19 +0200

    decoding seems to work

--- src/pcapreader/pcapreader.go
+++ src/pcapreader/pcapreader.go
@@ -4,12 +4,17 @@ import (
   "fmt"
   "github.com/google/gopacket"
   "github.com/google/gopacket/pcap"
+  "github.com/google/gopacket/layers"
 )
 
 func main() {
   if handle, err := pcap.OpenOffline("../../data/test.pcap"); err != nil {
     panic(err)
   } else {
+    layers.LinkTypeMetadata[12] = layers.EnumMetadata{
+      DecodeWith: layers.LayerTypeIPv4,
+      Name: "tun",
+    }
     packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
     for packet := range packetSource.Packets() {
       fmt.Println(packet)
