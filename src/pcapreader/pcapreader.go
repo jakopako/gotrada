49fe0df69fd7e846b4fc684a6ecd16d483cb8dc7
Jakob Dhondt <jakob.dhondt@switch.ch>
2018-08-07 11:36:08 +0200

    added test pcap

--- src/pcapreader/pcapreader.go
+++ src/pcapreader/pcapreader.go
@@ -7,12 +7,12 @@ import (
 )
 
 func main() {
-  if handle, err := pcap.OpenOffline("/Users/dhondt/pcaps/lako/lako.switch.ch.20170330.130000.000065.pcap"); err != nil {
+  if handle, err := pcap.OpenOffline("../../data/test.pcap"); err != nil {
     panic(err)
   } else {
     packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
     for packet := range packetSource.Packets() {
-      fmt.Println(packet) 
+      fmt.Println(packet)
     }
   }
 }
